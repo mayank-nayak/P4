0e96b41a84f8e8cd5917c1bb21b5f353625ebb1d
Mayank <mayank.nayak@hotmail.com>
2022-12-04 17:46:35 -0600

    added function prototypes

---
+++ mfs.c
@@ -0,0 +1,14 @@
+#include "mfs.h"
+#include "ufs.h"
+
+// FUNCTION PROTOTYPES
+
+int MFS_Init(char *hostname, int port);
+int MFS_Lookup(int pinum, char *name);
+int MFS_Stat(int inum, MFS_Stat_t *m);
+int MFS_Write(int inum, char *buffer, int offset, int nbytes);
+int MFS_Read(int inum, char *buffer, int offset, int nbytes);
+int MFS_Creat(int pinum, int type, char *name);
+int MFS_Unlink(int pinum, char *name);
+int MFS_Shutdown();
+
